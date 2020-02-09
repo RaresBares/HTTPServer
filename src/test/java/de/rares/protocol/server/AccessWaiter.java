badcc9f7ea036855d9e47a8e04c1875aabbff659
Rares <rsahleanu@gmail.com>
2020-02-09 09:58:06 +0100

    Correcture

--- src/test/java/de/rares/protocol/server/AccessWaiter.java
+++ src/test/java/de/rares/protocol/server/AccessWaiter.java
@@ -3,6 +3,7 @@ package de.rares.protocol.server;
 import java.awt.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -14,56 +15,47 @@ public class AccessWaiter extends Thread {
 
     @Override
     public void run() {
-        try {
-            while (true) {
+      while (true){
+          try {
 
-                Socket t = main.ss.accept();
-                DataInputStream dataInputStream = new DataInputStream(t.getInputStream());
-                boolean hsv;
+             Socket s = main.ss.accept();
+             DataOutputStream ostream = new DataOutputStream(s.getOutputStream());
+             DataInputStream istream = new DataInputStream(s.getInputStream());
+             Scanner sc = new Scanner(istream);
+             boolean faked = false;
+             String[] handshake = new String[3];
+             if(sc.nextLine() == "HSTART"){
 
-                if (dataInputStream.readUTF().trim().equalsIgnoreCase("HSTART")) {
+             }else{
+                 faked  = true;
+             }
+             for(int i = 0; i < handshake.length; i++){
+                 if(sc.hasNextLine()){
+                     String msg = sc.nextLine();
+                     if(msg != "HSTOP") {
+                         handshake[i] = msg;
+                     }else{
+                         if(i == 2){
 
-                    ArrayList<String> hs = new ArrayList<>();
-                    String msg = "";
-                    while ((msg = dataInputStream.readUTF()).trim() != "") {
-                        if (msg.contentEquals("HSTOP")) {
-                            msg = "";
+                         }else{
+                             faked = true;
+                         }
+                     }
+                 }else{
+                     faked = true;
+                 }
+             }
+              if(faked) {
+                  ostream.writeUTF("403 || Please trigger first the handshake!");
+              }else{
 
-                        } else {
-                            System.out.println(msg.trim());
-                            hs.add(msg.trim());
-
-
-                        }
-                    }
-
-
-
-                    hsv = verif(hs);
-                } else {
-                    new DataOutputStream(t.getOutputStream()).writeUTF("ERROR 403 | please trigger the Handshake!");
-                    dataInputStream.readUTF();
-                    dataInputStream.readUTF();
-                    dataInputStream.readUTF();
-
-
-                }
-                if (true) {
-
-                    MessageWaiter ms = new MessageWaiter();
-                    ms.t = t;
-                    ms.start();
+              }
 
 
-                } else {
-
-                }
-            }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+      }
     }
 
     public static boolean verif(ArrayList<String> hs) {
